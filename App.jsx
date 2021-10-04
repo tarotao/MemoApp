2b2706617c703625cf0b8d357afef0270ad28505
tarotaro <80757996+tarotao@users.noreply.github.com>
2021-10-04 20:57:29 +0900

    replace keybordAvoidingView

--- App.jsx
+++ App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-// import MemoEditlScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoEditlScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <MemoEditlScreen />
   );
 }

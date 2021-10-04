1de2c4af27cb1b6d852f5e9b04148be5f4e050ac
tarotaro <80757996+tarotao@users.noreply.github.com>
2021-10-04 22:23:59 +0900

    create login screen and sign up screen

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {
-  View, StyleSheet, TextInput, KeyboardAvoidingView,
+  View, StyleSheet, TextInput,
 } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
+import KeybordSafeView from '../components/KeyboardSafeView';
 
 export default function MemoEditlScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeybordSafeView style={styles.container} behavior="height">
       <AppBar />
       <View style={styles.inputContainaer}>
         <TextInput value="買い物リスト" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
-    </KeyboardAvoidingView>
+    </KeybordSafeView>
   );
 }
 
